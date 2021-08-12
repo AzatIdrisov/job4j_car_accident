711602de679fc5adeef7565f2f1ef0c7f14f5e9f
Azat Idrisov <azzzat72@yandex.ru>
2021-08-12 09:01:10 +0500

    1. Загрузка данных в вид [#295782]

--- src/main/java/ru/job4j/accident/control/IndexControl.java
+++ src/main/java/ru/job4j/accident/control/IndexControl.java
@@ -4,10 +4,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 public class IndexControl {
     @GetMapping("/")
     public String index(Model model){
+        model.addAttribute("user", "Azat Idrisov");
+        List<String> accidents = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            accidents.add("accident " + i);
+        }
+        model.addAttribute("accidents", accidents);
         return "index";
     }
 }
