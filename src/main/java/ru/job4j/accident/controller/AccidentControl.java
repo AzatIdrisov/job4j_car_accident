353670afb660b2263ea5717a4cbe858a434d4c80
Azat Idrisov <azzzat72@yandex.ru>
2021-08-14 11:21:22 +0500

    4. Form с композиционным объектом [#305522 #189057] refactor

--- src/main/java/ru/job4j/accident/controller/AccidentControl.java
+++ src/main/java/ru/job4j/accident/controller/AccidentControl.java
@@ -7,7 +7,6 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import ru.job4j.accident.model.Accident;
-import ru.job4j.accident.model.AccidentType;
 import ru.job4j.accident.model.Rule;
 import ru.job4j.accident.repository.*;
 
@@ -18,35 +17,39 @@ import java.util.*;
 public class AccidentControl {
 
     private final AccidentHibernate accidents;
+    private final AccidentTypesMem types;
+    private final RulesMem rules;
 
-    public AccidentControl(AccidentHibernate accidents) {
+    public AccidentControl(AccidentHibernate accidents, AccidentTypesMem types, RulesMem rules) {
         this.accidents = accidents;
+        this.types = types;
+        this.rules = rules;
     }
 
     @GetMapping("/create")
     public String create(Model model) {
-        model.addAttribute("types",new AccidentTypesMem().getAll());
-        model.addAttribute("rules", new RulesMem().getAll());
+        model.addAttribute("types",types.getAll());
+        model.addAttribute("rules", rules.getAll());
         return "accident/create";
     }
 
     @PostMapping("/save")
     public String save(@ModelAttribute Accident accident, HttpServletRequest req) {
         String[] ids = req.getParameterValues("rIds");
-        Set<Rule> rules = new HashSet<>();
+        Set<Rule> ruleSet = new HashSet<>();
         for(String ruleId: ids){
-            rules.add(new RulesMem().getById(Integer.parseInt(ruleId)));
+            ruleSet.add(rules.getById(Integer.parseInt(ruleId)));
         }
-        accident.setRules(rules);
+        accident.setRules(ruleSet);
         accidents.save(accident);
         return "redirect:/";
     }
 
     @GetMapping("/update")
     public String update(@RequestParam("id") int id, Model model) {
-        model.addAttribute("accident", AccidentMem.instOf().findById(id));
-        model.addAttribute("types",new AccidentTypesMem().getAll());
-        model.addAttribute("rules", new RulesMem().getAll());
+        model.addAttribute("accident", accidents.findById(id));
+        model.addAttribute("types",types.getAll());
+        model.addAttribute("rules", rules.getAll());
         return "accident/update";
     }
 }
